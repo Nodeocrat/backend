e156f4821b4c146dbdd9550d50834522933d3123
Ashley Phillips <ashley.phillips@nodeocrat.com>
2017-06-24 19:17:29 +0100

    Add events for node social chat room

--- NodeSocial/node-social.js
+++ NodeSocial/node-social.js
@@ -2,27 +2,27 @@ const EventTypes = require('./event-types.js');
 const config = require('../config.js');
 const API_ROOT = config.API_ROOT;
 const UTILS = config.UTILS;
+const OrderedHash = require(UTILS + '/OrderedHash.js');
 
 module.exports = function(app, io){
 
+  // {username, picUrl}
+  const lobbyPlayers = new OrderedHash();
+
   io.on('connection', function(socket){
 		const userId = socket.id;
-    const user = socket.request.user;
-    /*{"_id":"5935f387133c0278395c6735",
-       "password":"",
-       "email":"zukias@gmail.com",
-       "username":"狂猫",
-       "oauth": {
-         "google": {
-           "name":"狂猫 ",
-           "photoUrl":"https://lh3.googleusercontent.com/-REe7RRHpKz4/AAAAAAAAAAI/AAAAAAAAAUg/OAP7jbRCW_E/s96-c/photo.jpg","id":"116424390509412279867"}},"displayPicture":{"category":"google","value":"https://lh3.googleusercontent.com/-REe7RRHpKz4/AAAAAAAAAAI/AAAAAAAAAUg/OAP7jbRCW_E/s96-c/photo.jpg"
-          }
-        }
-      }*/
+    const user = socket.request.user || null;
+    if(!user)
+      return;
 
 		const addr = socket.request.connection.remoteAddress;
     const username = user.username;
 		console.log(`[INFO] ${username} opened new websocket session from ${addr}.`);
+
+    const basicUser = {username: username, picUrl: user.displayPicture.value, status: 'online'};
+    lobbyPlayers.insert(username, basicUser);
+    socket.emit(EventTypes.INIT_LOBBY, lobbyPlayers.toJSON());
+    io.emit(EventTypes.PLAYERS_JOINED, [basicUser]);
 
 		// TODO make sure that username not already in use
 		socket.on(EventTypes.SEND_MESSAGE, msg => {
@@ -36,12 +36,20 @@ module.exports = function(app, io){
         id: `${username}|${matchId}|${randomStr}`,
         text: msg.text
       };
-      console.log(`about to send back msg: ${JSON.stringify(message)}`)
       io.emit(EventTypes.CHAT_MESSAGE_RECEIVED, message);
     });
 
-    socket.on(EventTypes.DISCONNECT, () => {
+    socket.on(EventTypes.JOIN_GAME, info => {
+      console.log('in JOIN_GAME');
+      // GameList.get(info.id).join(socket);
+      socket.emit(EventTypes.INIT_GAME, {msg: "Hello from server"});
+    });
 
+    socket.on(EventTypes.DISCONNECT, () => {
+      lobbyPlayers.remove(username);
+
+      console.log(`[INFO] ${username} disconnected.`);
+      io.emit(EventTypes.PLAYERS_LEFT, [username]);
     });
   });
 }
